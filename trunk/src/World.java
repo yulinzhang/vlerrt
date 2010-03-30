aa6c57780ddaae6038ed4e97e969386699302adc
fm2097ad <fm2097ad@dc34fe40-da79-c0c0-7016-bb0b6becb33e>
2010-03-30 21:39:31 +0000

    Separation of GUI and RRTWorld... should it be called RRTworld instead?

--- trunk/src/World.java
+++ trunk/src/World.java
@@ -1,17 +1,19 @@
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 
 public interface World {
 
-	boolean collides(Point2D o, Point2D e);
+	public boolean collides(Point2D o);
+	public boolean collides(Point2D o, Point2D e);
 	
-	Point2D randomPoint();
+	public Point2D randomPoint();
+	public Point2D start();
+	public Point2D goal();
 	
-	Point2D start();
-	
-	Point2D goal();
-	
-	public void setSearchTree(Tree searchTree);
-	public void display();
+	public Iterable<Rectangle2D> obstacles();
+
+	public int height();
+	public int width();
 	
 }

1b1af4d7e4d9ca40819d561cfd3aa68f76c433df
fm2097ad <fm2097ad@dc34fe40-da79-c0c0-7016-bb0b6becb33e>
2010-04-20 20:49:58 +0000

    GUI now has D key listener for step. Removed Pausing classes.

--- trunk/src/gui/StatsGUI.java
+++ trunk/src/gui/StatsGUI.java
@@ -1,4 +1,5 @@
 package gui;
+
 import java.util.Random;
 
 import javax.swing.JFrame;
@@ -10,101 +11,100 @@ import javax.swing.JTable;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.general.DatasetChangeListener;
 import org.jfree.data.xy.AbstractIntervalXYDataset;
 import org.jfree.data.xy.IntervalXYDataset;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
-class PlotFactory
-{
-  private static XYDataset createDataset(String n, double[][] d) {
-        XYSeries xyseries = new XYSeries(n);
+class PlotFactory {
+	private static XYDataset createDataset(String n, double[][] d) {
+		XYSeries xyseries = new XYSeries(n);
 
-        for(double[] a:d)
-            xyseries.add(a[0], a[1]);
+		for(double[] a:d)
+			xyseries.add(a[0], a[1]);
 
-        return new XYSeriesCollection(xyseries);
-  }
+		return new XYSeriesCollection(xyseries);
+	}
 
-   private static XYDataset createDataset(String[] labels, double[][] d[]) {
-       if(labels.length != d.length)
-        throw new RuntimeException("Table Labels.size <> data.size");
+	private static XYDataset createDataset(String[] labels, double[][] d[]) {
+		if(labels.length != d.length)
+			throw new RuntimeException("Table Labels.size <> data.size");
 
-       XYSeriesCollection r = new XYSeriesCollection();
-       XYSeries s;
-       for(int i=0;i<labels.length;++i){
-        s = new XYSeries(labels[i]);
+		XYSeriesCollection r = new XYSeriesCollection();
+		XYSeries s;
+		for(int i=0;i<labels.length;++i){
+			s = new XYSeries(labels[i]);
 
-        for(double[] a:d[i])
-            s.add(a[0], a[1]);
+			for(double[] a:d[i])
+				s.add(a[0], a[1]);
 
-        r.addSeries(s);
-       }
+			r.addSeries(s);
+		}
 
-       return r;
-  }
+		return r;
+	}
 
-  public static JPanel createBarChart(String title,String x,String y,double[][] d) {
-        return new ChartPanel(ChartFactory.createXYLineChart(title, x, y,
-                createDataset("",d), PlotOrientation.VERTICAL, false, true, false));
-  }
+	public static JPanel createBarChart(String title,String x,String y,double[][] d) {
+		return new ChartPanel(ChartFactory.createXYLineChart(title, x, y,
+				createDataset("",d), PlotOrientation.VERTICAL, false, true, false));
+	}
 
-  public static JPanel createBarChart(String title,String x,String y,String[] labels,double[][] ...d) {
-        return new ChartPanel(ChartFactory.createXYLineChart(title, x, y,
-                createDataset(labels,d), PlotOrientation.VERTICAL, true, true, false));
-  }
+	public static JPanel createBarChart(String title,String x,String y,String[] labels,double[][] ...d) {
+		return new ChartPanel(ChartFactory.createXYLineChart(title, x, y,
+				createDataset(labels,d), PlotOrientation.VERTICAL, true, true, false));
+	}
 }
 
 
 @SuppressWarnings("serial")
-class Dataset extends AbstractIntervalXYDataset implements IntervalXYDataset
-{
-    private Double xStart[];
-    private Double xEnd[];
-    private Double yValues[];
-    private String name;
+class Dataset extends AbstractIntervalXYDataset implements IntervalXYDataset {
+	private Double xStart[];
+	private Double xEnd[];
+	private Double yValues[];
+	private String name;
 
-    public Dataset(String name,double[][] d) {
-        this.name=name;
-        xStart = new Double[d.length];
-        xEnd = new Double[d.length];
-        yValues = new Double[d.length];
+	public Dataset(String name,double[][] d) {
+		this.name=name;
+		xStart = new Double[d.length];
+		xEnd = new Double[d.length];
+		yValues = new Double[d.length];
 
-        for(int i=0;i<d.length;++i){
-            xStart[i] =  d[i][0];
-            xEnd[i] =    d[i][0]+1.0;
-            yValues[i] = d[i][1];
-        }
-    }
+		for(int i=0;i<d.length;++i){
+			xStart[i] =  d[i][0];
+			xEnd[i] =    d[i][0]+1.0;
+			yValues[i] = d[i][1];
+		}
+	}
 
-    public int getSeriesCount() { return 1; }
-    public Comparable<?> getSeriesKey(int i) { return name; }
-    public int getItemCount(int i) { return yValues.length; }
-    public Number getX(int i, int j) { return xStart[j]; }
-    public Number getY(int i, int j) { return yValues[j]; }
-    public Number getStartX(int i, int j) { return xStart[j]; }
-    public Number getEndX(int i, int j) { return xEnd[j]; }
-    public Number getStartY(int i, int j) { return yValues[j]; }
-    public Number getEndY(int i, int j) { return yValues[j]; }
+	public int getSeriesCount() { return 1; }
+	
+	public Comparable<?> getSeriesKey(int i) { return name; }
+	
+	public int getItemCount(int i)        { return yValues.length; }
+	public Number getX     (int i, int j) { return xStart[j]; }
+	public Number getY     (int i, int j) { return yValues[j]; }
+	public Number getStartX(int i, int j) { return xStart[j]; }
+	public Number getEndX  (int i, int j) { return xEnd[j]; }
+	public Number getStartY(int i, int j) { return yValues[j]; }
+	public Number getEndY  (int i, int j) { return yValues[j]; }
 
-    public void addChangeListener(DatasetChangeListener datasetchangelistener) {}
-    public void removeChangeListener(DatasetChangeListener datasetchangelistener) {}
+	//    public void addChangeListener(DatasetChangeListener datasetchangelistener) {}
+	//    public void removeChangeListener(DatasetChangeListener datasetchangelistener) {}
 
-    public static JPanel createBarChart(String title,String x,String y,String series,double[][] d) {
-        return new ChartPanel(ChartFactory.createXYBarChart(title,x, false,y,
-                new Dataset(series,d),PlotOrientation.VERTICAL, false, true, false));
-    }
+	public static JPanel createBarChart(String title,String x,String y,String series,double[][] d) {
+		return new ChartPanel(ChartFactory.createXYBarChart(title,x, false,y,
+				new Dataset(series,d),PlotOrientation.VERTICAL, false, true, false));
+	}
 }
 
 @SuppressWarnings("serial")
 public class StatsGUI extends JFrame {
-	
+
 	public StatsGUI(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(400,400);
-		
+
 		JTabbedPane tabs = new JTabbedPane();
 
 		//table
@@ -113,43 +113,43 @@ public class StatsGUI extends JFrame {
 		JTable table = new JTable(rows, columns);
 		table.setAutoscrolls(true);
 		tabs.addTab("table",new JScrollPane(table,
-                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-                JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS));
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS));
 
 		///
 		double[][] points = { {1,2,3,4} , {3,4,5,5}};
-		 tabs.addTab("bar",
-				 Dataset.createBarChart(
-	                        "asd",
-	                        "aqwd",
-	                        "qwe",
-	                        "qwe",
-	                        points));
-		
-		 Random rd = new Random();
-		 tabs.addTab("plot", PlotFactory.createBarChart(
-	                "title", "x-label", "y-label",
-	                new String[]{"a","b"},
-	            
-	                new double[][]{
-	                		{1,rd.nextDouble()}, // (x,y)
-	                		{2,rd.nextDouble()},
-	                		{3,rd.nextDouble()},
-	                		{4,rd.nextDouble()},
-	                		{5,rd.nextDouble()}
-	                },
+		tabs.addTab("bar",
+				Dataset.createBarChart(
+						"asd",
+						"aqwd",
+						"qwe",
+						"qwe",
+						points));
 
-	                new double[][]{
-	                		{1,rd.nextDouble()},
-	                		{2,rd.nextDouble()},
-	                		{3,rd.nextDouble()},
-	                		{4,rd.nextDouble()},
-	                		{5,rd.nextDouble()}
-	                }
-	                ));
+		Random rd = new Random();
+		tabs.addTab("plot", PlotFactory.createBarChart(
+				"title", "x-label", "y-label",
+				new String[]{"a","b"},
+
+				new double[][]{
+						{1,rd.nextDouble()}, // (x,y)
+						{2,rd.nextDouble()},
+						{3,rd.nextDouble()},
+						{4,rd.nextDouble()},
+						{5,rd.nextDouble()}
+				},
+
+				new double[][]{
+						{1,rd.nextDouble()},
+						{2,rd.nextDouble()},
+						{3,rd.nextDouble()},
+						{4,rd.nextDouble()},
+						{5,rd.nextDouble()}
+				}
+		));
 		add(tabs);
 	}
-	
+
 	public static void main(String[] args) throws Exception{
 		JFrame f = new StatsGUI();
 		f.setVisible(true);
